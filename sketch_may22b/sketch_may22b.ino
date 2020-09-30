1c28ec9f87a3477e133316ec2e8aea41c5783c7c
DJRaov <djraov2004@gmail.com>
2020-09-30 19:14:54 +0500

    Create sketch_may22b.ino
    Turns out I had the code already saved on my desktop, perfect.

---
+++ sketch_may22b/sketch_may22b.ino
@@ -0,0 +1,85 @@
+//Prerequisite for DHT lib
+#include <Adafruit_Sensor.h>
+
+//DHT libs
+#include <DHT.h>
+
+//LCD lib
+#include <LiquidCrystal.h>
+
+//degree symbol
+byte degree[8] =
+{
+  0b01000,
+  0b10100,
+  0b01000,
+  0b00011,
+  0b00100,
+  0b00100,
+  0b00011,
+  0b00000,
+};
+
+//define DHT things
+#define DHTPIN 11
+#define DHTPIN2 12
+#define DHTTYPE DHT11
+
+//init LCD
+LiquidCrystal lcd(7, 6, 2, 3, 4, 5);
+
+//define 2 functions for the DHTs so they can de addressed individually
+DHT dht(DHTPIN, DHTTYPE);
+DHT dht2(DHTPIN2, DHTTYPE);
+
+
+
+void setup()
+{
+  //set data line pins to pullup, since we want that shit to be reliable
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  
+  //init both DHTs
+  dht.begin();
+  dht2.begin();
+
+  //init LCD
+  lcd.begin(16, 2);
+
+  //create degree character
+  lcd.createChar(0, degree);
+}
+
+
+void loop()
+{
+  //defined here because fuck syntax
+  int h = dht.readHumidity();
+  float t = dht.readTemperature();
+  int h2 = dht2.readHumidity();
+  float t2 = dht2.readTemperature();
+
+  //LCD routine
+  //Internal (main) DHT
+  lcd.setCursor(0, 0);
+  lcd.print("Int");
+  lcd.setCursor(6, 0);
+  lcd.print(t);
+  lcd.write(byte(0));
+  lcd.setCursor(13, 0);
+  lcd.print(h);
+  lcd.print("%");
+
+  //External DHT
+  lcd.setCursor(0, 1);
+  lcd.print("Ext");
+  lcd.setCursor(6, 1);
+  lcd.print(t2);
+  lcd.write(byte(0));
+  lcd.setCursor(13, 1);
+  lcd.print(h2);
+  lcd.print("%");
+  //make that shit 60fps
+  delay(16);
+}
